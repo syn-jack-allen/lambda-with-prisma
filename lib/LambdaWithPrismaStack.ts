347d1078c5e42b7234ea43230765a0d2ec398471
Jack Allen <jack.allen@konsentus.com>
2023-06-19 18:55:49 +0100

    Added prisma layer that builds and bundles

--- lib/LambdaWithPrismaStack.ts
+++ lib/LambdaWithPrismaStack.ts
@@ -1,12 +1,35 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { DockerImage, Stack, StackProps } from 'aws-cdk-lib';
+import { Code, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaWithPrismaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambda = new NodejsFunction(this, 'LambdaFunction', {});
+    const bundledLayerVersion = new LayerVersion(this, 'BundledLayerVersion', {
+      code: Code.fromAsset('src/prisma-layer', {
+        bundling: {
+          image: DockerImage.fromBuild('src/prisma-layer'),
+          volumes: [
+            {
+              hostPath: join(__dirname, '../node_modules'),
+              containerPath: '/asset-input/node_modules',
+            },
+          ],
+          command: [
+            'bash',
+            '-c',
+            [
+              'esbuild ./client.ts --bundle --platform=node --outdir=/asset-output',
+              'cp node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node /asset-output',
+              'cp node_modules/.prisma/client/schema.prisma /asset-output',
+            ].join(' && '),
+          ],
+        },
+      }),
+    });
   }
 }
